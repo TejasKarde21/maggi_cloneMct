c5ee3f2d4a4011d76aa462f1738b1717112a6f2a
Tejas <137605358+TejasKarde21@users.noreply.github.com>
2023-07-14 14:44:39 +0530

    all the files are check twice

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {   
+        fontFamily:{
+          'Poppins': ['Poppins','sans-serif'],
+           'Inter': ['Inter', 'sans-serif' ]
+        },
+
+      colors: {
+      "primary": "#000000",
+      10: '#6a3a2a',
+      20: '#492c22',
+      30: '#3dba3b',
+     
+  },
+
+},
+  },
+  plugins: [
+     
+  ],
+}
